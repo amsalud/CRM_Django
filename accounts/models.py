1ef2e13a13575cdf43fd8779dcb8e828424e2814
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-02 17:38:17 -0400

    Add Order model

--- accounts/models.py
+++ accounts/models.py
@@ -11,8 +11,21 @@ class Customer(models.Model):
         return self.name
 
 class Product(models.Model):
+    CATEGORIES = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door')
+        )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORIES)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUSES = (
+       ('Pending', 'Pending'),
+       ('Out for delivery', 'Out for delivery'),
+       ('Delivered', 'Delivered')
+       )
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUSES)
