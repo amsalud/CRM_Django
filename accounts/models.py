9c443dcd77b9e0a422bb64c441767cafead3653d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-02 17:51:00 -0400

    Remove tag field from orders table and add it to products table

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORIES = (
         ('Indoor', 'Indoor'),
@@ -20,12 +26,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORIES)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-class Tag(models.Model):
-    name = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return self.name
+    tags = models.ManyToManyField(Tag)
 
 class Order(models.Model):
     STATUSES = (
@@ -37,4 +38,3 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUSES)
-    tags = models.ManyToManyField(Tag)
