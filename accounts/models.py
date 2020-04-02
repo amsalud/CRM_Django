8b9de85b5f09c34f34ba3511636e366b4119447e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-02 18:00:47 -0400

    Allow blank product descriptions in forms

--- accounts/models.py
+++ accounts/models.py
@@ -24,9 +24,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORIES)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUSES = (
