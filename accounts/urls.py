1f6d835e43f485136aa0611c6765dd0db9aafe2f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-03 20:10:33 -0400

    Add new route for order form

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
-    path('customer/<str:customer_id>/', views.customer, name="customer")
+    path('customer/<str:customer_id>/', views.customer, name="customer"),
+    path('create_order/', views.createOrder, name="create_order")
 ]
