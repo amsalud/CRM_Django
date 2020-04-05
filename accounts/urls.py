491cb113088698335e94b88a19215fd3a21a0878
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-04 21:53:32 -0400

    Add signout route

--- accounts/urls.py
+++ accounts/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
     path('login/', views.signin, name="login"),
+    path('logout/', views.signout, name="logout"),
     path('products/', views.products, name="products"),
     path('customer/<str:customer_id>/', views.customer, name="customer"),
     path('create_order/<str:customer_id>', views.createOrder, name="create_order"),
