6330b697d271519cd99e94d854139b86cdabe6b6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-05 20:46:17 -0400

    Add admin_only decorator

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -16,10 +16,21 @@ def allowed_users(allowed_roles=[]):
             group = None
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
-                print(request.user.groups)
             if group in allowed_roles:    
                 return view_func(request, *args, **kwargs)
             else:
                 return HttpResponse('You are not authorized to view this page')
         return wrapper_func 
-    return decorator   
+    return decorator
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'customer':    
+            return redirect('profile')
+        elif group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_func 
+   
